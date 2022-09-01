400308c077ae79fc605e6ad9969d94ee2228d559
Wojciech Cholewiński <wojciech.cholewinski@interia.pl>
2022-09-01 20:47:29 +0200

    added control ComboBox "typeOfInternet"

--- NewOrderPage.xaml.cs
+++ NewOrderPage.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,21 +24,34 @@ namespace Formatka
         public NewOrderPage()
         {
             InitializeComponent();
+            //Services services = new Services();
+            //typeOfInternet.ItemsSource = services.ToString();
+            List<TodoItem> items = new List<TodoItem>();
+            items.Add(new TodoItem() { Title = "Internet",});
+            items.Add(new TodoItem() { Title = "Numer komórkowy", });
+            items.Add(new TodoItem() { Title = "Pakiet internet + numer", });
+
+            typeOfInternet.ItemsSource = items;
         }
 
-        private void Internet_Button_Click(object sender, RoutedEventArgs e)
-        {
+        //private void Internet_Button_Click(object sender, RoutedEventArgs e)
+        //{
 
-        }
-        private void Mobile_Number_Button_Click(object sender, RoutedEventArgs e)
-        {
+        //}
+        //private void Mobile_Number_Button_Click(object sender, RoutedEventArgs e)
+        //{
 
-        }
+        //}
 
-        private void Package_Button_Click(object sender, RoutedEventArgs e)
-        {
+        //private void Package_Button_Click(object sender, RoutedEventArgs e)
+        //{
 
-        }
+        //}
+
+    }
+    public class TodoItem
+    {
+        public string Title { get; set; }
     }
     
 }
